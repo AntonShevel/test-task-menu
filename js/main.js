6b162dc1b6dfac6a84358de90b15fe1bb3e9517b
Шевель Антон Сергеевич <anton.shevel.01@privatbank.ua>
2014-06-23 16:09:40 +0300

    works

--- js/main.js
+++ js/main.js
@@ -1,65 +1,30 @@
-/*
-function getContent
-object Menu:
-- constructor
-- create dom
-- append
-- left/right
+var TestTask = TestTask || {};
 
-grunt!
- https://github.com/xxllexx/data-binding/blob/master/Gruntfile.js
- */
-//
+TestTask.Main = function()
+{
+    var menuElement = window.document.getElementById('menu'),
+        init = function() {
+            // get elements
+            var oReq = new XMLHttpRequest();
+            oReq.onload = reqListener;
+            oReq.open("get", "resource/items.json", true);
+            oReq.send();
 
-// get elements
-var oReq = new XMLHttpRequest();
-oReq.onload = reqListener;
-oReq.open("get", "resource/items.json", true);
-oReq.send();
-
-//success request
-function reqListener () {
-    var items = JSON.parse(this.responseText);
-    iterate(items);
-}
-
-// iterate
-function iterate(items) {
-
-    var menu = window.document.createDocumentFragment();
-
-    function makeDom(items, parent, container ,level) {
-        var item,
-            allowed = false,
-            ul = window.document.createElement('ul'),
-            li,
-            a;
-
-        ul.className = 'level-'+level;
-
-        for (var i = 0; items[i]; i += 1) {
-            item = items[i];
-
-            if (parent && item.parent === parent || !parent && !item.parent) {
-                allowed = true;
-                li = window.document.createElement('li');
-                a = window.document.createElement('a');
-                a.href = item.url;
-                a.innerHTML = item.name;
-                li.appendChild(a);
-                if (makeDom(items, item.id, li, level+1))
-                    li.className='has-sub';
-                ul.appendChild(li);
-
+            //success request
+            function reqListener () {
+                var items = JSON.parse(this.responseText),
+                    menu = new Menu(menuElement, items);
+                return menu;
             }
-        }
+        },
+        oPublic = {
+            init: init,
+            menuElement: menuElement
+        };
+    return oPublic;
+}();
 
-        if (allowed)
-            container.appendChild(ul);
+window.onload = function() {
+    TestTask.Main.init();
 
-        return allowed;
-    }
-    makeDom(items, false, menu, 0);
-
-    window.document.getElementById('menu').appendChild(menu);
-}
+};
